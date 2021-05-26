1a5d547f8dcdbc78eabafcee88edc36699fd8bd6
Cely Cueto <celinda.cueto@gmail.com>
2021-05-26 11:00:57 -0500

    nuevos cambios

--- js/function.js
+++ js/function.js
@@ -1,7 +1,6 @@
 
 function audios(){
-    var song = ["../assets/sounds/burro.mp3" , "../assets/sounds/vaca.mp3", 
-    "../assets/sounds/caballo.mp3", "../assets/sounds/conejo.mp3", "../assets/sounds/perro.mp3" ];
+    var song = ["./assets/sounds/burro.mp3" , "./assets/sounds/vaca.mp3", "./assets/sounds/caballo.mp3", "./assets/sounds/conejo.mp3", "./assets/sounds/perro.mp3" ];
 var audio = new Audio();
 var setSound = function() {
 var soundFile = escape(song[Math.floor(Math.random()*song.length)]);
