6257a43df6e82f3bcfb1f7a9c69c60b18bd6610d
celycueto-fx <celindacuetodelahoz07@gmail.com>
2021-05-26 12:34:29 -0500

    nuevos cambios

--- js/function.js
+++ js/function.js
@@ -9,32 +9,16 @@ var setSound = function() {
 var soundFile = escape(song[Math.floor(Math.random()*song.length)]);
 audio.setAttribute("src", soundFile);
 
-
+drawScore();
     audio.play();
      
 setSound();
 
 audio.addEventListener("ended", setSound, false);
 }
-
-var listaImagenes = document.getElementById("./assets/images/burro.png"); 
-// el id de donde quiere mostrar tu listado, te recomendaría poner los links directamente
-// no enquilombarte haciendo la concatenación con el ".jpg"
-
-var arreglo = ["./assets/images/burro.png","./assets/images/vaca.png","./assets/images/perro.png","./assets/images/conejo.png"]
-var NuevoArreglo = [];
-
-var auxiliar;
-// primer for
-for (var item = arreglo.length; item; item--) {
-    var numRandom = Math.floor(Math.random() * item);
-    auxiliar = arreglo[item - 1];
-    NuevoArreglo[item - 1] = arreglo[numRandom];
-    arreglo[numRandom] = auxiliar;
-}
-// segundo for
-for(var item = 0; item < arreglo.length ; item++){
-    var parrafo = document.createElement("p");
-    parrafo.innerHTML = NuevoArreglo[item];
-    listaImagenes.appendChild(parrafo);
+var score = 0;
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
 }
