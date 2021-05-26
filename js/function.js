32116abc64219a0712e259608efe7328a885f955
celycueto-fx <celindacuetodelahoz07@gmail.com>
2021-05-26 12:23:54 -0500

    nuevos cambios

--- js/function.js
+++ js/function.js
@@ -16,3 +16,25 @@ setSound();
 
 audio.addEventListener("ended", setSound, false);
 }
+
+var listaImagenes = document.getElementById("listaImagenes"); 
+// el id de donde quiere mostrar tu listado, te recomendaría poner los links directamente
+// no enquilombarte haciendo la concatenación con el ".jpg"
+
+var arreglo = ["./assets/images/burro.png","./assets/images/vaca.png","./assets/images/perro.png","./assets/images/conejo.png"]
+var NuevoArreglo = [];
+
+var auxiliar;
+// primer for
+for (var item = arreglo.length; item; item--) {
+    var numRandom = Math.floor(Math.random() * item);
+    auxiliar = arreglo[item - 1];
+    NuevoArreglo[item - 1] = arreglo[numRandom];
+    arreglo[numRandom] = auxiliar;
+}
+// segundo for
+for(var item = 0; item < arreglo.length ; item++){
+    var parrafo = document.createElement("p");
+    parrafo.innerHTML = NuevoArreglo[item];
+    listaImagenes.appendChild(parrafo);
+}
