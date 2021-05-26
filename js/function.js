898ef802d42098fded836fce6350cb956fea2db8
Cely Cueto <celinda.cueto@gmail.com>
2021-05-26 10:58:22 -0500

    nuevos cambios

---
+++ js/function.js
@@ -0,0 +1,16 @@
+
+function audios(){
+    var song = ["../assets/sounds/burro.mp3" , "../assets/sounds/vaca.mp3", 
+    "../assets/sounds/caballo.mp3", "../assets/sounds/conejo.mp3", "../assets/sounds/perro.mp3" ];
+var audio = new Audio();
+var setSound = function() {
+var soundFile = escape(song[Math.floor(Math.random()*song.length)]);
+audio.setAttribute("src", soundFile);
+audio.play();
+};
+
+// set initial source
+setSound();
+// set a new source, when current sound is done playing
+audio.addEventListener("ended", setSound, false);
+}
